3ab1f336d52254a0274d774dacf422146a073af0
Pedro Victor <stormsamurai1@yahoo.com.br>
2019-10-23 22:44:33 -0300

    Adicionada estrutura de repetição

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,29 +5,32 @@ use rand::Rng;
 fn main() {
     println!("Welcome to the Guessing Game!!!");
 
-
-    println!("Please input your guess: ");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    println!("You guessed: {}", guess);
-
-    //Transforma a String em numero de 32 bits
-    //sem sinal, captura erro se não for um número
-    let guess: u32 = guess.trim().parse()
-                .expect("Please type a number!");
-
     //Gera números aleatórios em um range de 1 a 101
     let secret_number = rand::thread_rng().gen_range(1,101);
 
-    //Utiliza das arms do match para comparar, um a um,
-    //que padrão combina com o número e a entrada
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+
+    loop {
+        println!("Please input your guess: ");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        println!("You guessed: {}", guess);
+
+        //Transforma a String em numero de 32 bits
+        //sem sinal, captura erro se não for um número
+        let guess: u32 = guess.trim().parse()
+                    .expect("Please type a number!");
+
+
+        //Utiliza das arms do match para comparar, um a um,
+        //que padrão combina com o número e a entrada
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
