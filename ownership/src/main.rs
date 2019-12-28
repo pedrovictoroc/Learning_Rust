a386213d5925224515b0ace2acc43223cef8cf76
stormsamurai1 <stormsamurai1@yahoo.com.br>
2019-12-28 14:23:32 -0300

    reference and borrowing resume and examples

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    
+    /*    
     //Testando cópia para variáveis de tamanho fixo
     let x = 5;
     let y = x;
@@ -7,7 +7,7 @@ fn main() {
     println!("Y está disponível: {}", y);
     println!("X está disponível: {}", x);
     
-/*
+
     //Testando cópia com variáveis mutáveis
     let mut s1 = String::from("Hello");
     s1.push_str(", world!");
@@ -25,7 +25,26 @@ fn main() {
     let s1 = String::from("Hello");
     let s2 = takes_and_gives_ownership(s1);
     println!("{}",s2);
+
+    //Testando conceito de empréstimo
+    let s1 = String::from("hello");
+    let len = get_len(&s1);
+    println!("{}",len);
+    
+
+    //Testando mudança em algo passado por referencia
+    //Para que possamos alterar algo passado por referencia, devemos declarar na passagem
+    //a variável como do tipo mutable
+    let mut s1 = String::from("hello");
+    change(&mut s1);
+    println!("{}",s1);
     */
+
+    let mut s1 = String::from("ola mundo");
+
+    let world = first_world(&s1);
+    
+    println!("{}",world);
 }
 
 
@@ -35,4 +54,24 @@ fn take_ownership(s: String){
 
 fn takes_and_gives_ownership(s: String) -> String {
     s
+}
+
+fn get_len(s: &String) -> usize{
+    s.len()
+}
+
+fn change(s: &mut String){
+    s.push_str(", world!");
+}
+
+fn first_world(s: &String) -> &str{
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
